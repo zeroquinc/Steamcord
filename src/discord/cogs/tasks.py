389355bf60c7551035bf163a311722a38cded139
Zeroquinc <desiler@protonmail.com>
2024-05-26 16:48:50 +0200

    feature: add more globals

--- src/discord/cogs/tasks.py
+++ src/discord/cogs/tasks.py
@@ -3,7 +3,7 @@ from discord.ext import tasks, commands
 
 from src.discord.embed import EmbedBuilder
 from src.steam.functions import get_all_achievements, create_embed_description
-from config.globals import STEAM_API_KEY1, STEAM_API_KEY2
+from config.globals import STEAM_API_KEY1, STEAM_API_KEY2, ACHIEVEMENT_CHANNEL
 from utils.custom_logger import logger
 
 class TasksCog(commands.Cog):
@@ -13,11 +13,18 @@ class TasksCog(commands.Cog):
 
     @tasks.loop(minutes=60)
     async def process_achievements(self):
+        logger.info("Start Achievement task")
+
         user_ids = [76561198035515815, 76561198840513734]
         api_keys = [STEAM_API_KEY1, STEAM_API_KEY2]
-        channel = self.bot.get_channel(1052967176828616724)
+        channel = self.bot.get_channel(ACHIEVEMENT_CHANNEL)
 
         all_achievements = await get_all_achievements(user_ids, api_keys)
+
+        if all_achievements:
+            logger.info("Found achievements")
+        else:
+            logger.info("No achievements found")
 
         for game_achievement, user_achievement, user_game, user in all_achievements:
             description = create_embed_description(user_achievement, user_game)
