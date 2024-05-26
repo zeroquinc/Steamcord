e20e41195d4cd2e0407d76f83e864731de0a252b
Zeroquinc <desiler@protonmail.com>
2024-05-26 02:45:38 +0200

    chore: break big functions down into smaller ones

--- src/discord/bot.py
+++ src/discord/bot.py
@@ -3,9 +3,8 @@ from discord.ext import commands
 from datetime import datetime
 import asyncio
 
-from src.steam.functions import get_achievement_description
+from src.steam.functions import get_all_achievements, create_embed_description
 from src.discord.embed import EmbedBuilder
-from src.steam.functions import check_recently_played_games
 from config.globals import STEAM_API_KEY1, STEAM_API_KEY2
 from utils.custom_logger import logger
 
@@ -41,25 +40,12 @@ class DiscordBot:
             user_ids = [76561198035515815, 76561198840513734]
             api_keys = [STEAM_API_KEY1, STEAM_API_KEY2]
 
-            all_achievements = []
-            for user_id, api_key in zip(user_ids, api_keys):
-                achievements = await check_recently_played_games(user_id, api_key)
-                all_achievements.extend(achievements)
+            all_achievements = await get_all_achievements(user_ids, api_keys)
 
-            # Sort achievements by unlocktime of the corresponding user_achievement
-            all_achievements.sort(key=lambda pair: datetime.strptime(pair[1].unlocktime, '%d/%m/%y %H:%M:%S'))
-
-            # Add achievements to the embed
             for game_achievement, user_achievement, user_game, user in all_achievements:
-                # Use the game achievement in the description
-                achievement_info = get_achievement_description(user_game.appid, user_achievement.name)
-                if achievement_info:
-                    description = f"**{user_achievement.name}** <:silver:1242467048035192955> **{achievement_info['percentage']}**\n{achievement_info['description']}\n\n[{user_game.name}]({user_game.url})"
-                else:
-                    description = f"**{user_achievement.name}**\n\n{user_game.name}"
+                description = create_embed_description(user_achievement, user_game)
                 embed = EmbedBuilder(description=description)
                 embed.set_thumbnail(url=game_achievement.icon)
                 embed.set_footer(text=f"{user.summary.personaname} • {user_achievement.unlocktime}", icon_url=user.summary.avatarfull)
-                # Send the embed
                 await embed.send_embed(ctx.channel)
                 await asyncio.sleep(1)
--- src/steam/functions.py
+++ src/steam/functions.py
@@ -10,84 +10,118 @@ from utils.custom_logger import logger
 
 DATE_FORMAT = '%d/%m/%y %H:%M:%S'
 
-async def check_recently_played_games(user_id, api_key):
-    try:
-        # Create a SteamClient instance with the provided API key
-        client = SteamClient(api_key)
-        # Create a User instance and fetch user's owned games
-        user = client.user(user_id)
-        user.get_user_summaries()
-        user.get_owned_games()
-        # Get the current timestamp and format it to match unlocktime
-        current_time = datetime.now().strftime(DATE_FORMAT)
-        achievements = []
-        for user_game in user.owned_games:
-            # Check if the game has been played within the last 24 hours
-            if user_game.last_played != "Unknown":
-                last_played_date = datetime.strptime(user_game.last_played, DATE_FORMAT)
-                if datetime.strptime(current_time, DATE_FORMAT) - last_played_date <= timedelta(days=20):
-                    game_instance = client.game()
-                    game_instance.get_game_achievements(user_game.appid)
-                    # Fetch the user's achievements for the game
-                    user.get_user_achievements(user_game.appid)
-                    # Iterate over the user's achievements
-                    for user_achievement in user.achievements:
-                        # Check if the achievement was unlocked
-                        if user_achievement.achieved == 1:
-                            # Iterate over the game's achievements
-                            for game_achievement in game_instance.achievements:
-                                # Check if the names match
-                                if game_achievement.displayname == user_achievement.name:
-                                    # Check if the achievement was unlocked within the last 24 hours
-                                    unlocktime = datetime.strptime(user_achievement.unlocktime, DATE_FORMAT)
-                                    if unlocktime and datetime.strptime(current_time, DATE_FORMAT) - unlocktime <= timedelta(days=20):
-                                        # Add the game achievement and the corresponding user_achievement to the achievements list
-                                        achievements.append((game_achievement, user_achievement, user_game, user))
-        return achievements
-    except Exception as e:
-        logger.error(f"Error processing achievements for user {user_id}: {e}")
-        return []
-
 def normalize_whitespace(text):
     return re.sub(r'\s+', ' ', text).strip()
 
-def scrape_all_achievements(app_id):
+def get_achievement_page(app_id):
     url = f"https://completionist.me/steam/app/{app_id}/achievements"
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    # Find all achievements
-    achievements = soup.find_all('strong')
-    # Create a dictionary of achievement names and descriptions
-    achievement_dict = {}
-    for achievement in achievements:
-        name = normalize_whitespace(achievement.text)
-        description_span = achievement.find_next_sibling('span', class_='text-muted')
-        if description_span:
-            description = normalize_whitespace(description_span.text)
-            percentage_tds = achievement.parent.find_next_siblings('td', class_='text-center')
-            if len(percentage_tds) > 1 and percentage_tds[1].find('small'):
-                percentage = normalize_whitespace(percentage_tds[1].find('small').text)
-            else:
-                percentage = 'Unknown'
-            achievement_dict[name] = {'description': description, 'percentage': percentage}
-    # Define the file path
+    return BeautifulSoup(response.text, 'html.parser')
+
+def get_achievement_info(achievement):
+    name = normalize_whitespace(achievement.text)
+    description_span = achievement.find_next_sibling('span', class_='text-muted')
+    description = normalize_whitespace(description_span.text) if description_span else None
+    percentage_tds = achievement.parent.find_next_siblings('td', class_='text-center')
+    percentage = normalize_whitespace(percentage_tds[1].find('small').text) if len(percentage_tds) > 1 and percentage_tds[1].find('small') else 'Unknown'
+    return name, {'description': description, 'percentage': percentage}
+
+def write_achievements_to_file(achievement_dict, app_id):
     file_path = os.path.join(os.path.dirname(__file__), 'data', f'achievements_{app_id}.json')
-    # Write the dictionary to a file
     with open(file_path, 'w') as f:
         json.dump(achievement_dict, f)
+
+def scrape_all_achievements(app_id):
+    soup = get_achievement_page(app_id)
+    achievements = soup.find_all('strong')
+    achievement_dict = {}
+    for achievement in achievements:
+        name, info = get_achievement_info(achievement)
+        if name:
+            achievement_dict[name] = info
+    write_achievements_to_file(achievement_dict, app_id)
     return achievement_dict
 
-def get_achievement_description(app_id, achievement_name):
-    # Define the file path
+def load_achievements_from_file(app_id):
     file_path = os.path.join(os.path.dirname(__file__), 'data', f'achievements_{app_id}.json')
-    # Try to load the achievements from a file
     try:
         with open(file_path, 'r') as f:
             achievement_dict = json.load(f)
         logger.info(f'Loaded achievements for app {app_id} from JSON file')
     except FileNotFoundError:
-        # If the file doesn't exist, scrape the achievements
         logger.info(f'File not found, scraping achievements for app {app_id}')
         achievement_dict = scrape_all_achievements(app_id)
-    # Return the description of the given achievement
+    return achievement_dict
+
+def get_achievement_description(app_id, achievement_name):
+    achievement_dict = load_achievements_from_file(app_id)
     return achievement_dict.get(achievement_name)
+
+async def get_user_games(user_id, api_key):
+    client = SteamClient(api_key)
+    user = client.user(user_id)
+    user.get_user_summaries()
+    user.get_owned_games()
+    return user
+
+async def get_recently_played_games(user):
+    current_time = datetime.now().strftime(DATE_FORMAT)
+    recently_played_games = []
+    for user_game in user.owned_games:
+        if user_game.last_played != "Unknown":
+            last_played_date = datetime.strptime(user_game.last_played, DATE_FORMAT)
+            if datetime.strptime(current_time, DATE_FORMAT) - last_played_date <= timedelta(days=20):
+                recently_played_games.append(user_game)
+    return recently_played_games
+
+async def get_game_achievements(user_game, user, client):
+    game_instance = client.game()
+    game_instance.get_game_achievements(user_game.appid)
+    user.get_user_achievements(user_game.appid)
+    return game_instance.achievements, user.achievements
+
+async def find_matching_achievements(user_achievement, game_achievements, current_time, user_game, user):
+    matching_achievements = []
+    for game_achievement in game_achievements:
+        if game_achievement.displayname == user_achievement.name:
+            unlocktime = datetime.strptime(user_achievement.unlocktime, DATE_FORMAT)
+            if unlocktime and datetime.strptime(current_time, DATE_FORMAT) - unlocktime <= timedelta(days=20):
+                matching_achievements.append((game_achievement, user_achievement, user_game, user))
+    return matching_achievements
+
+async def get_recent_achievements(game_achievements, user_achievements, user_game, user):
+    current_time = datetime.now().strftime(DATE_FORMAT)
+    achievements = []
+    for user_achievement in user_achievements:
+        if user_achievement.achieved == 1:
+            achievements.extend(await find_matching_achievements(user_achievement, game_achievements, current_time, user_game, user))
+    return achievements
+
+async def get_all_achievements(user_ids, api_keys):
+    all_achievements = []
+    for user_id, api_key in zip(user_ids, api_keys):
+        achievements = await check_recently_played_games(user_id, api_key)
+        all_achievements.extend(achievements)
+    all_achievements.sort(key=lambda pair: datetime.strptime(pair[1].unlocktime, '%d/%m/%y %H:%M:%S'))
+    return all_achievements
+
+def create_embed_description(user_achievement, user_game):
+    if achievement_info := get_achievement_description(user_game.appid, user_achievement.name):
+        return f"**{user_achievement.name}** <:silver:1242467048035192955> **{achievement_info['percentage']}**\n{achievement_info['description']}\n\n[{user_game.name}]({user_game.url})"
+    else:
+        return f"**{user_achievement.name}**\n\n{user_game.name}"
+
+async def check_recently_played_games(user_id, api_key):
+    try:
+        client = SteamClient(api_key)
+        user = await get_user_games(user_id, api_key)
+        recently_played_games = await get_recently_played_games(user)
+        achievements = []
+        for user_game in recently_played_games:
+            game_achievements, user_achievements = await get_game_achievements(user_game, user, client)
+            recent_achievements = await get_recent_achievements(game_achievements, user_achievements, user_game, user)
+            achievements.extend(recent_achievements)
+        return achievements
+    except Exception as e:
+        logger.error(f"Error processing achievements for user {user_id}: {e}")
+        return []
