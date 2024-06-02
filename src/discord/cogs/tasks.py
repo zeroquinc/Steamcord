551bc1e15499316fcd38247ea691452d47e400c7
Zeroquinc <desiler@protonmail.com>
2024-06-02 21:58:04 +0200

    feature: add user and api keys to env

--- src/discord/cogs/tasks.py
+++ src/discord/cogs/tasks.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 from src.discord.embed import EmbedBuilder
 from src.steam.functions import get_all_achievements, create_embed_description_footer
-from config.globals import STEAM_API_KEY1, STEAM_API_KEY2, ACHIEVEMENT_CHANNEL
+from config.globals import STEAM_API_KEY, ACHIEVEMENT_CHANNEL, STEAM_ID
 from utils.custom_logger import logger
 
 class TasksCog(commands.Cog):
@@ -16,14 +16,14 @@ class TasksCog(commands.Cog):
     async def process_achievements(self):
         logger.info("Start Achievement task")
 
-        user_ids = [76561198035515815, 76561198840513734]
-        api_keys = [STEAM_API_KEY1, STEAM_API_KEY2]
+        user_ids = STEAM_ID
+        api_keys = STEAM_API_KEY
         channel = self.bot.get_channel(ACHIEVEMENT_CHANNEL)
 
         all_achievements = await get_all_achievements(user_ids, api_keys)
 
-        # Sort all achievements by unlock time in descending order
-        all_achievements.sort(key=lambda a: datetime.strptime(a[1].unlocktime, "%d/%m/%y %H:%M:%S"), reverse=False)
+        # Sort all achievements by unlock time in descending order and then by order they were achieved in ascending order
+        all_achievements.sort(key=lambda a: (datetime.strptime(a[1].unlocktime, "%d/%m/%y %H:%M:%S"), a[4]/a[5]), reverse=False)
 
         if all_achievements:
             logger.info("Found achievements")
